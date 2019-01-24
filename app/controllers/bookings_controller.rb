5e09e8974ea599b31b416fd5b307d399d2340950
Courtney Hollenkamp <cmhollenkamp@gmail.com>
2019-01-24 11:22:08 -0500

    updating booking associations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,18 +2,23 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new(flight_id: params[:flight][:flight])
     @passengers_num = params[:flight][:passengers_num].to_i
-    #params[:flight][:passengers_num].to_i.times do
-      #@booking.passenger.build
-    #end
   end
 
   def create
     index = 0
     @passengers = []
     @bookings = []
+    #original
+    #params[:booking][:passengers].each do |passenger|
+      #@passengers << Passenger.where(passenger_params(index)).first_or_initialize
+      #@bookings << Flight.find(params[:booking][:flight]).bookings.build(passenger: @passengers[index])
+      #index += 1
+    #end
+    @flight = Flight.find(params[:booking][:flight])
+    @booking = @flight.bookings.build
     params[:booking][:passengers].each do |passenger|
       @passengers << Passenger.where(passenger_params(index)).first_or_initialize
-      @bookings << Flight.find(params[:booking][:flight]).bookings.build(passenger: @passengers[index])
+      @booking = Flight.find(params[:booking][:flight]).bookings.build(passenger: @passengers[index])
       index += 1
     end
     if @bookings.each(&:save)
